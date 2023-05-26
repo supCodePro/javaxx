a10fa14c87f47d38b6b529913611f824ebb39466
x1226958711@outlook.com <86350437+codefarmer19@users.noreply.github.com>
2023-05-26 14:46:39 +0800

    equals

--- Person.java
+++ Person.java
@@ -4,6 +4,9 @@ public class Person {
      * 定义一个人类 根据类型创建出来有哪些属性 以及哪些行为
      * 变量有两种 一个局部标量一个成员变量
      * */
+    public int Id;
+    public String name1;
+
     String name;//姓名 郝美女
     int age;//年龄 17
     char gender;//性别 女
@@ -14,6 +17,18 @@ public class Person {
     byte zijie;//字节类型
     /* 顺便复习八大数据类型 要牢记 */
     OtherPerson ops;//在成员变量里存其他信息
+    @Override
+    public  boolean equals(Object obj){
+        if(this==obj)
+            return true;
+        if(obj instanceof Person){
+            //如果是person类型的转成person
+            Person ps=(Person) obj;
+            if(this.Id==ps.Id&&this.name1.equals((ps.name1)))//是STRING类型不能用==用equals
+                return true;
+        }
+        return false;
+    }
     class Person2{
         String name1;//姓名 郝美女
     }
